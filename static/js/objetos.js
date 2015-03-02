6ffeace512275491f43acac00ef6ec63805f839e
Andres Carrillo <andresjch2804@gmail.com>
2015-03-01 21:03:11 -0500

    try cath eval

--- static/js/objetos.js
+++ static/js/objetos.js
@@ -1,7 +1,7 @@
-
-var obj = prompt("referencia u objeto: ");
-
-renderTemplate(obj);
+function fnPrompt(){
+	var obj = prompt("referencia u objeto: ");
+	renderTemplate(obj);
+};
 
 function inspeccionar(obj)
 {
@@ -9,21 +9,21 @@ function inspeccionar(obj)
 
   for (var property in obj)
   {
-    if (typeof obj[property] == 'function')
-    {
-      var inicio = obj[property].toString().indexOf('function');
-      var fin = obj[property].toString().indexOf(')')+1;
-      var propertyValue=obj[property].toString().substring(inicio,fin);
-      msg[msg.length] = {'type' : (typeof obj[property]), 'name' : property, 'value' : propertyValue};
-    }
-    else if (typeof obj[property] == 'unknown')
-    {
-      msg[msg.length] = {'type' : 'unknown', 'name' : property, value : 'unknown'};
-    }
-    else
-    {
-      msg[msg.length] ={'type' : (typeof obj[property]), 'name' : property, 'value' : obj[property]};
-    }
+	if (typeof obj[property] == 'function')
+	{
+	  var inicio = obj[property].toString().indexOf('function');
+	  var fin = obj[property].toString().indexOf(')')+1;
+	  var propertyValue=obj[property].toString().substring(inicio,fin);
+	  msg[msg.length] = {'type' : (typeof obj[property]), 'name' : property, 'value' : propertyValue};
+	}
+	else if (typeof obj[property] == 'unknown')
+	{
+	  msg[msg.length] = {'type' : 'unknown', 'name' : property, value : 'unknown'};
+	}
+	else
+	{
+	  msg[msg.length] ={'type' : (typeof obj[property]), 'name' : property, 'value' : obj[property]};
+	}
   }
   return msg;
 };
@@ -34,14 +34,26 @@ function activateTemplate(id) {
 };
 
 function renderTemplate(obj){
-	var tbody = document.querySelector(".property__body");
-	var propertys = inspeccionar(eval(obj));
+	var ttitle = document.querySelector(".propertys__title");
+	var tbody = document.querySelector(".propertys__body");
+	var propertys;
 
-	for (var i = 0; i < propertys.length; i++) {
-		var clone = activateTemplate("#template--property");
-		clone.querySelector("[data-property]").innerHTML = propertys[i].name;
-		clone.querySelector("[data-type]").innerHTML = propertys[i].type;
-		clone.querySelector("[data-value]").innerHTML = propertys[i].value;
-		tbody.appendChild(clone);
-	};
+	try{
+		propertys = inspeccionar(eval(obj));
+		tbody.innerHTML = "";
+		ttitle.innerHTML = obj;
+		for (var i = 0; i < propertys.length; i++) {
+			var clone = activateTemplate("#template--property");
+			clone.querySelector("[data-property]").innerHTML = propertys[i].name;
+			clone.querySelector("[data-type]").innerHTML = propertys[i].type;
+			clone.querySelector("[data-value]").innerHTML = propertys[i].value;
+			tbody.appendChild(clone);
+		};
+
+	}catch(err){
+		ttitle.innerHTML = "";
+		tbody.innerHTML = "<tr><td colspan='3'>Propiedad no disponible</td></tr>"
+	}
+
+
 };
